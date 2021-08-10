d304af6cfb1da692cad7d1f48f6f05ebcd3238be
raluca.titieni <raluca.titieni@fortech.ro>
2021-08-10 20:03:31 +0300

    changed controller

--- src/main/java/main/PencilController.java
+++ src/main/java/main/PencilController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/pencils")
+@RequestMapping
 public class PencilController {
 
     @Autowired
